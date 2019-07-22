90eebdaaf7af5e7ed1f9eeb377e57f5092944e8e
alejandro <alejandro.valdesval@gmail.com>
2019-07-22 15:18:34 -0400

    vgg working

--- models/vgg19_fcn.py
+++ models/vgg19_fcn.py
@@ -9,41 +9,47 @@ import tensorflow as tf
 from losses import *
 
 def getVGG19FCN():
-	n_classes = 1
-	stride = 32
+    tf.reset_default_graph()
+    sess = tf.Session()
+    K.clear_session()
 
-	input_tensor = layers.Input(shape=(256, 256, 1))
-	base_model = VGG19(weights=None, include_top=False, input_tensor=input_tensor)
+    # load ResNet
+    n_classes = 1
+    stride = 32
 
-	#32
-	x = base_model.get_layer('block5_pool').output
-	x = layers.Conv2D(n_classes,1,padding = 'same', kernel_initializer='he_normal')(x)	
-	x = layers.Conv2DTranspose(n_classes, 3, name='up_32', strides=(stride), activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.Conv2D(n_classes, 3, padding = 'same', activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.Conv2D(n_classes, 3, name='pred_32', padding = 'same', activation='sigmoid', kernel_initializer = 'he_normal')(x)
-	pred_32 = x
+    input_tensor = layers.Input(shape=(256, 256, 1))
+    base_model = VGG19(weights=None, include_top=False, input_tensor=input_tensor)
 
-	#16
-	x = base_model.get_layer('block4_pool').output
-	x = layers.Conv2D(n_classes,1,padding = 'same', kernel_initializer='he_normal')(x)	
-	x = layers.Conv2DTranspose(n_classes, 3, name='up_16', strides=(stride//2), activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.Conv2D(n_classes, 3, padding = 'same', activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.add([x, pred_32])
-	x = layers.Conv2D(n_classes, 3, name='pred_16', padding = 'same', activation='sigmoid', kernel_initializer = 'he_normal')(x)
-	pred_16 = x
+    # add classifier
+    x = base_model.get_layer('block5_pool').output
 
-	#8
-	x = base_model.get_layer('block3_pool').output
-	x = layers.Conv2D(n_classes,1,padding = 'same', kernel_initializer='he_normal')(x)	
-	x = layers.Conv2DTranspose(n_classes, 3, name='up_8', strides=(stride//4), activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.Conv2D(n_classes, 3, padding = 'same', activation='relu', kernel_initializer = 'he_normal')(x)
-	x = layers.add([x, pred_16])
-	x = layers.Conv2D(n_classes, 3, name='pred_8', padding = 'same', activation='sigmoid', kernel_initializer = 'he_normal')(x)
-	pred_8 = x
+    ## add 32s upsampler
+    x = layers.UpSampling2D(size=(stride), interpolation='bilinear')(x)
+    x = layers.Conv2D(n_classes, 3, name = 'pred_up_32', padding = 'same',
+            activation='sigmoid', kernel_initializer='he_normal')(x)
+    pred_32s = x
 
-	model = Model(input=base_model.input,output=x)
-	model.compile(optimizer = Adam(lr = 1e-4),
-            loss = binary_crossentropy,
-            metrics = [dice_coef])
+    # 16s
+    x = base_model.get_layer('block4_pool').output
+    x = layers.UpSampling2D(name='upsampling_16',size=(stride//2), interpolation='bilinear')(x)
+    x = layers.Conv2D(n_classes, 3, name = 'pred_up_16', padding = 'same', kernel_initializer='he_normal')(x)
 
-	return model
+    # merge classifiers
+    x = layers.add([x, pred_32s])
+    x = layers.Activation('sigmoid')(x)
+    pred_16s = x
+
+    x = base_model.get_layer('block3_pool').output
+    x = layers.UpSampling2D(name='upsampling_8',size=(stride//4), interpolation='bilinear')(x)
+    x = layers.Conv2D(n_classes, 3, name = 'pred_up_8',padding = 'same', kernel_initializer='he_normal')(x)
+
+    # merge classifiers
+    x = layers.add([x, pred_16s])
+    x = layers.Activation('sigmoid')(x)
+
+    model = Model(input=base_model.input,output=x)
+    model.compile(optimizer = Adam(lr = 1e-4),
+        loss = binary_crossentropy,
+        metrics = [dice_coef])
+
+    return model
