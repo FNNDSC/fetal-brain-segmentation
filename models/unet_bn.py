10f69d73c1c11363e92daea8e813419300fb1b40
Alejandro <alejandro.valdesval@gmail.com>
2019-08-06 10:17:28 -0400

    added different losses

--- models/unet_bn.py
+++ models/unet_bn.py
@@ -12,7 +12,37 @@ from keras.losses import binary_crossentropy
 
 import tensorflow as tf
 
-def getUnetBN():
+def binary_focal_loss(gamma=2., alpha=.25):
+    """
+    Binary form of focal loss.
+      FL(p_t) = -alpha * (1 - p_t)**gamma * log(p_t)
+      where p = sigmoid(x), p_t = p or 1 - p depending on if the label is 1 or 0, respectively.
+    References:
+        https://arxiv.org/pdf/1708.02002.pdf
+    Usage:
+     model.compile(loss=[binary_focal_loss(alpha=.25, gamma=2)], metrics=["accuracy"], optimizer=adam)
+    """
+    def binary_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred:  A tensor resulting from a sigmoid
+        :return: Output tensor.
+        """
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+
+        epsilon = K.epsilon()
+        # clip to prevent NaN's and Inf's
+        pt_1 = K.clip(pt_1, epsilon, 1. - epsilon)
+        pt_0 = K.clip(pt_0, epsilon, 1. - epsilon)
+
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) \
+               -K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+
+    return binary_focal_loss_fixed
+
+def getUnetBN(loss_function = 'BCE'):
 
     tf.reset_default_graph()
     sess = tf.Session()
@@ -91,8 +121,19 @@ def getUnetBN():
 
     model = Model(inputs = inputs, outputs = conv10)
 
+    if loss_function == 'BCE':
+        loss = binary_crossentropy
+    elif loss_function == 'focal'
+        loss = binary_focal_loss(gamma=1., alpha=0.6)
+    elif loss_function == 'dice':
+        loss = dice_loss
+    elif loss_function == 'BCE_DICE':
+        loss = bce_dice_loss
+
+    print(loss_function)
+
     model.compile(optimizer = Adam(lr = 1e-4),
-                        loss=binary_crossentropy, #[binary_focal_loss(gamma=1., alpha=0.6)],
+                        loss=loss,
                         metrics = [dice_coef])
 
     return model
